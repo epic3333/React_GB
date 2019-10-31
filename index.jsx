a77bcab3b412e61cc855789c5b357166cb8c0f39
epic3333 <39667600+epic3333@users.noreply.github.com>
2019-10-31 15:47:17 +0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* const element = React.createElement(
+    'h1',
+    { className: 'element' },
+    "Кажется, работает React",
+); */
+
+const messages = ['Hi', 'how are you'];
+
+const Message = (props) => <div className="message">{ props.text }</div>
+
+const sendMessage = () => {
+    messages.push('Okay');
+    ReactDOM.render(
+        <MessageField messages={ messages } />,
+        document.getElementById('root'));
+};
+
+const MessageField = (props) => <div>
+    <h1>Чат</h1>
+    { props.messages.map(message => <Message text={ message } />) }
+    <button onClick={ sendMessage }>Send</button>
+</div>;
+
+ReactDOM.render(
+    <MessageField messages={ messages } />,
+    document.getElementById('root'),
+);
---
+++ script.js
@@ -0,0 +1,3 @@
+export const script = () => {
+    console.log('it is script.js');
+}
